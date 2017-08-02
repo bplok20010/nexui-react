25bbc54b6aeb2ca987c2675aa5ed5fb5381bb5ff
nobo <505931977@qq.com>
2017-08-02 16:40:48 +0800

    add animate

--- src/Portal/Portal.js
+++ src/Portal/Portal.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import {findDOMNode} from 'react-dom';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import {getDom, createContainer, removeContainer} from './util';
 
 export default class Portal extends React.Component {
 	static propTypes = {
 		children: PropTypes.node.isRequired,
 		selector: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+		animate: PropTypes.shape({
+			appear: PropTypes.func,
+			leave: PropTypes.func	
+		}),
 	};
 
 	static defaultProps = {
 		selector: 'body',
+		className: '',
+		prefixCls: 'nex-portal'
 	};
 
 	componentDidMount() {
-		const { selector, onCreate } = this.props
+		const { selector, onCreate, prefixCls, className, animate={} } = this.props
 		const elm = getDom(selector);
 		this._container = createContainer(elm);
 		this.renderPortal();
+		
+		if( animate.appear ) {
+			animate.appear(findDOMNode(this._instance));	
+		}
 	}
 	
 	renderPortal(){
+		const { prefixCls, className } = this.props
 		const children = React.Children.only(this.props.children);
+		
+		this._container.className = classNames( prefixCls, className );
 		
 		this._instance = ReactDOM.unstable_renderSubtreeIntoContainer(
 			this,
@@ -39,7 +54,18 @@ export default class Portal extends React.Component {
 	}
 
 	componentWillUnmount() {
-		this.removePortal();
+		const { animate={} } = this.props;
+		
+		const done = ()=>{
+			this.removePortal();		
+		}
+		
+		if( animate.leave ) {
+			animate.leave(findDOMNode(this._instance), done)	
+		} else {
+			done();
+		}
+		
 	}
 
 	componentDidUpdate() {
--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -15,13 +15,22 @@ export default class Popup extends React.Component {
 		destroyOnClose: PropTypes.bool,
 		visible: PropTypes.bool,
 		fixed: PropTypes.bool,
-		onClose: PropTypes.func,
+		onMaskClick: PropTypes.func,
+		popupAnimate: PropTypes.shape({
+			appear: PropTypes.func,
+			leave: PropTypes.func	
+		}),
+		maskAnimate: PropTypes.shape({
+			appear: PropTypes.func,
+			leave: PropTypes.func	
+		}),
 	};
 
 	static defaultProps = {
 		prefixCls: 'nex-popup',
 		mask: false,
-		fixed: true,
+		fixed: false,
+		destroyOnClose: true,
 		visible: false
 	};
 	
@@ -29,7 +38,10 @@ export default class Popup extends React.Component {
 		super(props);
 		
 		this.state = {
-			visible: props.visible
+			visible: props.visible,
+			isInit: true,
+			isHidden: false,
+			enableAnim: true
 		}	
 	}
 	
@@ -47,7 +59,7 @@ export default class Popup extends React.Component {
 	getPosition(type = 1){
 		let pos, dir;
 		const popup = this.refs.popup;
-		const {of, my, at, collision, using, within} = this.props;
+		let {of, my, at, collision, using, within} = this.props;
 		const config = {
 			using: function(p, d){
 				pos = p;
@@ -68,6 +80,10 @@ export default class Popup extends React.Component {
 			config.within = within;	
 		}
 		
+		if( typeof of === 'function' ) {
+			of = of();	
+		}
+		
 		position(popup, of || document.body, config);	
 		
 		return type == 1 ? pos : ( type == 2 ? dir : _assign(pos, dir) );
@@ -79,15 +95,13 @@ export default class Popup extends React.Component {
 	}
 	
 	componentDidMount(){
-		if( this.props.visible ) {
+		const { visible } = this.props;
+		
+		this.state.isInit = false;
+		
+		if( visible ) {
 			this.doShow();
 		}
-	}
-	
-	componentDidUpdate(){
-		if( this.props.visible ) {
-			this.doShow();
-		}	
 	}
 	
 	componentWillReceiveProps({visible}){
@@ -96,13 +110,70 @@ export default class Popup extends React.Component {
 		});
 	}
 	
+	componentDidUpdate(){
+		const { visible } = this.props;
+		const { popup, mask } = this.refs;
+		if( visible ) {
+			if( this.state.isHidden ) {
+				$(popup).show();
+				$(mask).show();		
+			}
+			
+			this.doShow();
+			
+			if( this.state.isHidden ) {
+				this.state.isHidden = false;
+				this.animateAppear();
+			}
+		} else if( this.state.isHidden ) {
+			this.animateLeave(null, ()=> {
+				$(popup).hide();
+				$(mask).hide();	
+			});	
+		}
+	}
+	
+	componentWillUnmount(){
+		if( this.state.isHidden ) {
+			this.state.enableAnim = false;
+		}
+	}
+	
+	animateAppear = () => {
+		const {popupAnimate, maskAnimate} = this.props;
+		
+		if( popupAnimate && popupAnimate.appear ) {
+			popupAnimate.appear(this.refs.popup);	
+		}
+		
+		if( maskAnimate && maskAnimate.appear ) {
+			maskAnimate.appear(this.refs.mask);	
+		}
+	}
+	
+	animateLeave = (node, done)=>{
+		const {popupAnimate, maskAnimate, visible} = this.props;
+		
+		if( this.state.enableAnim && popupAnimate && popupAnimate.leave ) {
+			popupAnimate.leave(this.refs.popup, done);	
+		} else {
+			done();	
+		}
+		
+		if( this.state.enableAnim && maskAnimate && maskAnimate.leave ) {
+			maskAnimate.leave(this.refs.mask, ()=>{});	
+		}
+	}
+	
 	doShow(){
 		this.setPosition();	
 	}
 	
-	close(){
-		const onClose = this.props.onClose;
-		if( onClose ) onClose();
+	handleMaskClick=(e)=>{
+		const {onMaskClick} = this.props;
+		if( onMaskClick ) {
+			onMaskClick(e);	
+		}	
 	}
 	
 	getMaskComponent(){
@@ -114,23 +185,21 @@ export default class Popup extends React.Component {
 			} );
 		
 		return (
-			<div ref="mask" className={classes}></div>
+			<div ref="mask" className={classes} onClick={this.handleMaskClick}></div>
 		);	
 	}
 	
 	getRenderComponent(){
-		const {prefixCls, position,className, style={}, fixed, mask} = this.props;
-		const { visible } = this.state;
+		const {prefixCls, position,className, destroyOnClose, style={}, fixed, mask, animate={}} = this.props;
+		let { visible, isInit } = this.state;
 		const classes = classNames(prefixCls, className, fixed ? prefixCls + '-fixed' : '');
 		
 		const others = omit(this.props, [
 			'prefixCls',
 			'position',
 			'className',
-			'style',
 			'visible',
 			'fixed',
-			'onClose',
 			'of',
 			'my',
 			'at',
@@ -138,17 +207,28 @@ export default class Popup extends React.Component {
 			'using',
 			'within',
 			'mask',
+			'destroyOnClose',
+			'popupAnimate',
+			'maskAnimate'
 		]);
 		
 		const popup = (
 			<div>
 				{mask ? this.getMaskComponent() : null}
-				<div ref="popup" className={classes} style={style} tabIndex={-1} {...others}></div>
+				<div ref="popup" className={classes} tabIndex={-1} {...others}></div>
 			</div>
 		);
 		
+		if( !visible && !destroyOnClose && !isInit ) {
+			visible = true;
+			this.state.isHidden = true;	
+		}
+		
 		return visible ? (
-			<Portal>{popup}</Portal>
+			<Portal animate={{
+				appear: this.animateAppear,
+				leave: this.animateLeave	
+			}}>{popup}</Portal>
 		) : null;
 	}
 
