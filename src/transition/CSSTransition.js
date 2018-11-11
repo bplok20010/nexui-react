b1492b2fa6d31d786a37c2f5c7578f0cc939726b
nobo <505931977@qq.com>
2018-11-11 10:39:42 +0800

    已修改        src/transition/CSSTransition.js 已修改        src/transition/Transition.js

--- src/transition/CSSTransition.js
+++ src/transition/CSSTransition.js
@@ -1,26 +1,27 @@
+import React, { Component } from 'react';
 import CSSTransition from 'react-transition-group/CSSTransition';
 import Transition from './Transition';
 
 class NCSSTransition extends CSSTransition {
-	static propTypes = {...CSSTransition.propTypes}
-	
-	render() {
-		const props = { ...this.props };
-		
-		delete props.classNames;
-		
-		return (
-			<Transition
-				{...props}
-				onEnter={this.onEnter}
-				onEntered={this.onEntered}
-				onEntering={this.onEntering}
-				onExit={this.onExit}
-				onExiting={this.onExiting}
-				onExited={this.onExited}
-			/>
-		);
-	}
+    static propTypes = { ...CSSTransition.propTypes }
+
+    render() {
+        const props = { ...this.props };
+
+        delete props.classNames;
+
+        return (
+            <Transition
+                {...props}
+                onEnter={this.onEnter}
+                onEntered={this.onEntered}
+                onEntering={this.onEntering}
+                onExit={this.onExit}
+                onExiting={this.onExiting}
+                onExited={this.onExited}
+            />
+        );
+    }
 }
 
 export default NCSSTransition;
